853bd1d9b0db6a54d04cdd3a1a8e590410120732
yahsya24 <yayahya2004@gmail.com>
2023-04-07 15:17:23 +0700

    kuirikulum

---
+++ app/Views/pages/shevi_kurikulum/page1.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Kurikulum</title>
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <!-- Bootstrap Icon - short-cut(bina-bsicon) -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+
+</head>
+
+<style>
+    body {
+        background-image: url('https://assets.ayobandung.com/crop/0x0:0x0/750x500/webp/photo/2022/07/20/3684434291.jpg');
+        background-size: cover;
+        padding: 30px;
+        height: 10vh;
+        background-position: top center;
+    }
+</style>
+
+<body class="bg-light">
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-primary">
+        <div class="container">
+            <a class="navbar-brand" href="#">KURIKULUM</a>
+            <ul class="navbar-nav ">
+                <li class="navbar-nav">
+                    <button class="btn btn-light text-black ">
+                        <a href="<?= base_url('/matkul') ?>">Matakuliah</a>
+                    </button>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+
+
+
+
+    <!-- Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</body>
+
+</html>
