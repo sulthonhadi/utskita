abaa7244084012e8cb40b51472baa0b445dca869
zipul <azismuhammad366@gmail.com>
2023-04-08 01:29:44 +0700

    seminar

---
+++ app/Views/seminar/Hal3.php
@@ -0,0 +1,97 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Seminar</title>
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <!-- Bootstrap Icon - short-cut(bina-bsicon) -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+
+    <!-- Datatable -->
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs5/dt-1.13.1/r-2.4.0/datatables.min.css" />
+    <!-- Select2 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/css/select2.min.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.min.css" />
+    <!-- JQUERY -->
+    <script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
+</head>
+
+<body>
+
+    <h2 class="text-center mt-4"> <strong>Ngonten Doang, Bisa Jualan?</strong> </h2>
+    <div class="container">
+        <div class="mt-5">
+            <p> <b>Tanggal:</b> 13 April 2023 </p>
+            <p><b>Tempat:</b> IdeaHub, Voza Tower lt 20, Surabaya</p>
+            <p><b>HTM:</b> Rp 150.000</p>
+            </p>
+        </div>
+
+        <div class="d-block w-50 mx-auto my-4">
+            <img src="asset/LoketMC-13-April-2023.jpg" class="w-100" alt="">
+
+        </div>
+        <P style="text-align: justify;">
+        <p>Ngonten Dulu Atau Jualan Dulu? </p>
+
+        <p>Sepenting apa sih digital marketing diterapkan dalam bisnis?
+            Apa iya omzet bisa langsung meroket?
+            Nanti kerjaan tim cuma bikin konten doang, malah nggak jualan?</p>
+
+        <p>Hmm, padahal kalau tahu tips & tricks nya, digital marketing itu bisa jadi strategi paling low cost, high impact, lho!💸</p>
+
+        <p>Contoh konkritnya, seperti salah satu platform media sosial yang sekarang sedang naik daun. Ya, Tiktok! Asal kita tahu algoritmanya, dijamin deh, seluruh konten Anda pasti bisa jadi sumber omzet! 💰</p>
+
+        <p>Nah, dalam Masterclass Connectpedia, kita akan kupas tuntas semuanya, mulai dari analisa algoritma sampai campaign anti boncos.😉</p>
+
+        <p>Yuk, daftar sekarang di Masterclass Digital Series!</p>
+
+        <p>Ngonten Doang, Bisa Jualan?</p>
+
+        <p>🗓️ Kamis, 13 April 2023 <br>
+            🕑 14.00-16.00 WIB <br>
+            🌐Hybrid
+        </p>
+        <p>Onsite di IdeaHub – Voza Tower, Surabaya.
+            Online by Zoom</p>
+        <p>Speaker :</p>
+
+        <p>Johan Kartahadikusuma
+            CMO Tribeca.id Digital Branding Activation</p>
+
+        <p>Book yours, NOW! Slot terbatas‼️</p>
+
+        <p>Klik link berikut untuk registrasi</p>
+        <p>👇🏻 <br>
+            https://bit.ly/MC-DIGITAL
+        </p>
+        <p>RSVP 📲</p>
+        <p>Komunitas Bisnis Connectpedia</p>
+        <p>WA: http://wa.me/6281357622902</p>
+        Telegram: t.me/connectpedia
+        </P>
+
+    </div>
+
+    <!-- Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <!-- Datatable -->
+    <script type="text/javascript" src="https://cdn.datatables.net/v/bs5/dt-1.13.1/r-2.4.0/datatables.min.js"></script>
+    <!-- Sweet Alert2 -->
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <!-- Select2 -->
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/js/select2.full.min.js"></script>
+
+    <!-- <script> 
+ $$('#select-field').select2({ 
+ theme: 'bootstrap-5' 
+ }); 
+ </script> -->
+</body>
+
+</html>
