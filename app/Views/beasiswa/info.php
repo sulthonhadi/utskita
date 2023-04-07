9e0e3d8cde5147c297029f473dcf6e05f3643337
mjazaia <118255055+mjazaia@users.noreply.github.com>
2023-04-08 01:42:42 +0700

    beasiswa

---
+++ app/Views/beasiswa/info.php
@@ -0,0 +1,57 @@
+<?php
+include("nav.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS v5.2.1 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+<style>
+.card{
+  width: auto;
+  margin: auto;
+}
+</style>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+            <div class="card-body">
+                <div class="card mt-5" style="width: 23rem;">
+                        <img src="./gambar1.jpg" class="card-img-top" alt="...">
+                            <!-- <p class="card-text">informasi</p> -->
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+            <div class="card-body">
+                <div class="card mt-5" style="width: 23rem;">
+                        <img src="./gambar2.jpg" class="card-img-top" alt="...">
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+            <div class="card-body">
+                <div class="card mt-5" style="width: 23rem;">
+                        <img src="./gambar3.jpg" class="card-img-top" alt="...">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
---
+++ app/Views/beasiswa/nav.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+
+    <style>
+body{
+    background-image: url(./gambar4.jpg);
+    background-size: cover;
+    background-attachment:fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+    
+   
+}
+.navbar {
+  overflow: hidden;
+  background-color:#191970 ;
+  
+  top: 0;
+  width: 100%;
+}
+.navbar a {
+  float: left;
+  display: block;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 7px 10px;
+  text-decoration: none;
+  font-size: 17px;
+  
+}
+.navbar a:hover {
+  background: #ddd;
+  color: black;
+}
+
+
+</style>
+</head>
+
+<body>
+     
+<nav class="navbar navbar-expand-sm shadow">
+        <div class="container-fluid">
+            <a class="nav-link text-white" href="index.php">Beasiswa</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" 
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto mt-2 mt-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#">Home</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+</body>
+</html>
