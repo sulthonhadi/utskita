0967be8731fe1c9580d8da53a1cbd9224dab870f
Tiannisme <bungkornet@gmail.com>
2023-04-08 14:59:58 +0700

    plu

--- app/Views/p_ukm/bem.php
+++ app/Views/p_ukm/bem.php
@@ -18,13 +18,13 @@
         <?php
         $siswa = [
             [
-                'id' => '1', 'nim' => '220342112', 'nama' => 'Hadi', 'ttl' => '2001-07-31', 'jeniskelamin' => 'Laki - Laki', 'agama' => 'Islam', 'prodi' => 'Informatika', 'fakultas' => 'Teknologi Informasi', 'alamat' => 'Jombang'
+                'id' => '1', 'nim' => '220342112', 'nama' => 'Hadi', 'ttl' => '2001-07-31', 'jeniskelamin' => 'Laki - Laki', 'agama' => 'Islam', 'prodi' => 'Informatika', 'fakultas' => 'Teknologi Informasi', 'alamat' => 'Jombang', 'jabatan' => 'ketua'
             ],
             [
-                'id' => '2', 'nim' => '228974632', 'nama' => 'Rizuki', 'ttl' => '2003-05-19', 'jeniskelamin' => 'Laki - Laki', 'agama' => 'Islam', 'prodi' => 'Informatika', 'fakultas' => 'Teknologi Informasi', 'alamat' => 'Jombang'
+                'id' => '2', 'nim' => '228974632', 'nama' => 'Rizuki', 'ttl' => '2003-05-19', 'jeniskelamin' => 'Laki - Laki', 'agama' => 'Islam', 'prodi' => 'Informatika', 'fakultas' => 'Teknologi Informasi', 'alamat' => 'Jombang', 'jabatan' => 'sekretaris'
             ],
             [
-                'id' => '3', 'nim' => '222254212', 'nama' => 'Supri', 'ttl' => '2001-08-16', 'jeniskelamin' => 'Laki - Laki', 'agama' => 'Islam', 'prodi' => 'Informatika', 'fakultas' => 'Teknologi Informasi', 'alamat' => 'Jombang'
+                'id' => '3', 'nim' => '222254212', 'nama' => 'Supri', 'ttl' => '2001-08-16', 'jeniskelamin' => 'Laki - Laki', 'agama' => 'Islam', 'prodi' => 'Informatika', 'fakultas' => 'Teknologi Informasi', 'alamat' => 'Jombang', 'jabatan' => 'bendahara'
             ],
         ]
         ?>
@@ -42,6 +42,7 @@
                         <th>PRODI</th>
                         <th>FAKULTAS</th>
                         <th>ALAMAT</th>
+                        <th>JABATAN</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -58,6 +59,7 @@
                             <td><?php echo $mahasiswa['prodi'] ?></td>
                             <td><?php echo $mahasiswa['fakultas'] ?></td>
                             <td><?php echo $mahasiswa['alamat'] ?></td>
+                            <td><?php echo $mahasiswa['jabatan'] ?></td>
                         </tr>
                     <?php
                     }
